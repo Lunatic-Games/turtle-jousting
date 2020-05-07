8439e899b719b1c5d2c34ccb3f19493e0eef923c
Joe <joe.zlonicky@gmail.com>
2020-05-07 14:56:56 -0700

    Fixed spawning

--- game/game.gd
+++ game/game.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 const player_scene = preload("res://player/player.tscn")
+var player_spawn_x = 50
+var player_spawn_y = 50
 
 func _ready():
 	pass
@@ -10,4 +12,6 @@ func add_player(number, net_id, data = {}):
 	new_player.name = "Player" + str(number)
 	new_player.load_data(data)
 	new_player.set_network_master(net_id)
+	new_player.position = Vector2(player_spawn_x, player_spawn_y)
+	player_spawn_x += 50
 	add_child(new_player)
